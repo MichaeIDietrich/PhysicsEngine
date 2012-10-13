db566da7591e23f54bdb82515c854fcdbc81fa38
Micha <Micha@Micha-PC>
2012-10-13 19:00:55 +0200

    added the possibility to zoom configuarations will be saved and loaded automatically now it is possible to select an object

--- PhysicsApplication/src/de/engineapp/Configuration.java
+++ PhysicsApplication/src/de/engineapp/Configuration.java
@@ -1,5 +1,114 @@
 package de.engineapp;
 
-public class Configuration {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
+public class Configuration implements Serializable
+{
+    private static final long serialVersionUID = -5115836553640371734L;
+    
+    
+    private static Configuration instance = null;
+    
+    private double zoom = 1.0;
+    private boolean showGrid = false;
+    private boolean snapToGrid = false; // not yet implemented
+    private boolean showInfo = false;
+    
+    
+    public static Configuration getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Configuration();
+        }
+        
+        return instance;
+    }
+    
+    
+    /** do not change - not yet fully implemented */
+    public double getZoom()
+    {
+        return zoom;
+    }
+    
+    public void setZoom(double zoom)
+    {
+        this.zoom = zoom;
+    }
+    
+    
+    /** weather a grid is drawn on the canvas */
+    public boolean isShowGrid()
+    {
+        return showGrid;
+    }
+    
+    public void setShowGrid(boolean showGrid)
+    {
+        this.showGrid = showGrid;
+    }
+    
+    
+    public boolean isSnapToGrid()
+    {
+        return snapToGrid;
+    }
+    
+    public void setSnapToGrid(boolean snapToGrid)
+    {
+        this.snapToGrid = snapToGrid;
+    }
+    
+    
+    public boolean isShowInfo()
+    {
+        return showInfo;
+    }
+    
+    public void setShowInfo(boolean showInfo)
+    {
+        this.showInfo = showInfo;
+    }
+    
+    
+    // will be propably replaced by some xml file
+    public static void load()
+    {
+        File configFile = new File("config.dat");
+        
+        if (configFile.exists())
+        {
+            try (FileInputStream   fis = new FileInputStream(configFile);
+                 ObjectInputStream ois = new ObjectInputStream(fis))
+            {
+                instance = (Configuration) ois.readObject();
+            }
+            catch (IOException | ClassNotFoundException ex)
+            {
+                ex.printStackTrace();
+            }
+            
+        }
+    }
+    
+    
+    public static void save()
+    {
+        try (FileOutputStream   fos = new FileOutputStream("config.dat");
+             ObjectOutputStream oos = new ObjectOutputStream(fos))
+        {
+            oos.writeObject(getInstance());
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 }
--- PhysicsApplication/src/de/engineapp/controls/ZoomSlider.java
+++ PhysicsApplication/src/de/engineapp/controls/ZoomSlider.java
@@ -1,5 +1,8 @@
 package de.engineapp.controls;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.text.DecimalFormat;
 
 import javax.swing.JLabel;
@@ -21,23 +24,57 @@ public class ZoomSlider extends JPanel
     
     public ZoomSlider()
     {
+        this(1.0);
+    }
+    
+    
+    public ZoomSlider(double initValue)
+    {
+        this.setLayout(null);
+        this.setMaximumSize(new Dimension(235, 30));
+        this.setMinimumSize(new Dimension(235, 30));
         
-        slider = new JSlider(1, 50, 10);
+        slider = new JSlider(-9, 18, 0)
+        {
+            private static final long serialVersionUID = 170776352557001522L;
+            
+            @Override
+            public void paint(Graphics g)
+            {
+                super.paint(g);
+                
+                g.setColor(Color.BLACK);
+                // 0.1
+                g.drawLine(7, 20, 7, 24);
+                // 1.0
+                g.drawLine(69, 20, 69, 24);
+                // 2.0
+                g.drawLine(138, 20, 138, 24);
+                // 10.0
+                g.drawLine(192, 20, 192, 24);
+            }
+        };
+        setValue(initValue);
+        
+        slider.setLocation(0, 0);
+        slider.setSize(200, 30);
         slider.setFocusable(false);
         slider.setPaintTicks(true);
         slider.setSnapToTicks(true);
         slider.setMinorTickSpacing(1);
-        slider.setMajorTickSpacing(10);
         slider.addChangeListener(new ChangeListener()
         {
             @Override
             public void stateChanged(ChangeEvent e)
             {
-                label.setText(FORMATTER.format(getValue()));
+                label.setText(FORMATTER.format(getValue()) + "x");
             }
         });
         
         label = new JLabel();
+        label.setLocation(200, 0);
+        label.setSize(35, 30);
+        label.setText(FORMATTER.format(getValue()) + "x");
         
         this.add(slider);
         this.add(label);
@@ -45,12 +82,42 @@ public class ZoomSlider extends JPanel
     
     public double getValue()
     {
-        return slider.getValue() / 10.0;
+        if (slider.getValue() < 0)
+        {
+            return slider.getValue() / 10.0 + 1.0;
+        }
+        else if (slider.getValue() == 0)
+        {
+            return 1.0;
+        }
+        else if (slider.getValue() < 10)
+        {
+            return slider.getValue() / 10.0 + 1.0;
+        }
+        else
+        {
+            return slider.getValue() - 8.0;
+        }
     }
     
     public void setValue(double value)
     {
-        slider.setValue((int) Math.round(value * 10.0));
+        if (value < 1.0)
+        {
+            slider.setValue((int) (value * 10.0 - 10.0));
+        }
+        else if (value == 1.0)
+        {
+            slider.setValue(0);
+        }
+        else if (value < 2.0)
+        {
+            slider.setValue((int) (value * 10.0 - 1.0));
+        }
+        else
+        {
+            slider.setValue((int) (value + 8.0));
+        }
     }
     
     public void addChangeListener(ChangeListener listener)
