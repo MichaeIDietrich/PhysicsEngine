ad0e7af375c56320fe6e31392aff7ca8500bf574
Micha <micha@Micha-Laptop.de>
2012-10-30 16:24:41 +0100

    fixed a bug, when the user tried to add a square to the scene -> nullpointer exception

--- PhysicsApplication/src/de/engineapp/visual/Square.java
+++ PhysicsApplication/src/de/engineapp/visual/Square.java
@@ -3,7 +3,7 @@ package de.engineapp.visual;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.*;
-import java.util.Collection;
+import java.util.*;
 
 import de.engine.math.Vector;
 import de.engineapp.PresentationModel;
@@ -13,12 +13,14 @@ public class Square extends de.engine.objects.Square implements IDrawable, ISele
     private String name;
     private Color color = Color.GREEN;
     private Color border = null;
+    private HashMap<String, IDrawable> decorMap;
     
     
     public Square(PresentationModel model, Vector position, Vector corner)
     {
         super(position, corner);
         name = "Objekt " + this.id;
+        decorMap = new HashMap<>();
     }
     
     
@@ -26,6 +28,7 @@ public class Square extends de.engine.objects.Square implements IDrawable, ISele
     {
         super(position, new Vector(radius, radius));
         name = "Objekt " + this.id;
+        decorMap = new HashMap<>();
     }
     
     
@@ -112,6 +115,6 @@ public class Square extends de.engine.objects.Square implements IDrawable, ISele
     @Override
     public Collection<IDrawable> getDecorSet()
     {
-        return null;
+        return decorMap.values();
     }
 }
