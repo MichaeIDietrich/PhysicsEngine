b9eaf233a8f5fedd115be8abf59ebcc8885d4e2a
ille90 <tim.illner@it2010.ba-leipzig.de>
2012-11-06 19:18:35 +0100

    PropertySpinner hinzugefügt, PropertyPanel aktualisiert

---
+++ PhysicsApplication/src/de/engineapp/controls/PropertySpinner.java
@@ -0,0 +1,38 @@
+package de.engineapp.controls;
+
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeListener;
+
+public class PropertySpinner extends JSpinner
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -7629422754400062262L;
+    
+    //private SpinnerNumberModel spinnerModel = null;
+    
+    public PropertySpinner(double start, double min, double max, double step, ChangeListener cl)
+    {
+        super(new SpinnerNumberModel(start,min,max,step));
+        System.out.println("model: " + this.getModel());
+        this.addChangeListener(cl);
+    }
+    
+    
+    //@Override
+    public Double getValue()
+    {
+        System.out.println("get");
+        return (Double) this.getModel().getValue();
+    }
+
+    @Override
+    public void setValue(Object value)
+    {
+        this.getModel().setValue(value);
+    }
+    
+}
