edbb9adf85065d21ed5c89a029a490007cda2eff
Johannes Hackel <johannes.wolle@folclassik.de>
2012-11-11 17:30:49 +0100

    handle pinned objects

--- PhysicsEngine/src/de/engine/math/PhysicsEngine2D.java
+++ PhysicsEngine/src/de/engine/math/PhysicsEngine2D.java
@@ -3,46 +3,47 @@ package de.engine.math;
 import de.engine.colldetect.CollisionDetector;
 import de.engine.environment.EnvProps;
 import de.engine.environment.Scene;
-import de.engine.objects.Circle;
 import de.engine.objects.ObjectProperties;
 
-public class PhysicsEngine2D {
+public class PhysicsEngine2D
+{
     
-	private Scene scene;
-
-	CollisionDetector collDetector;
-
-	public PhysicsEngine2D() {
-		this.collDetector = new CollisionDetector(scene);
-		//
-	}
-
-	public void Rotation() {
-		//
-	}
-
-	public void Translation() {
-		//
-	}
-
-	public void setScene(Scene scene) {
-		this.scene = scene;
-		this.collDetector = new CollisionDetector(scene);
-	}
-
-	// here starts the entry point for all the physical calculation
-	public void calculateNextFrame(double deltaTime) 
-	{
-		EnvProps.deltaTime(deltaTime);
-
-		collDetector.checkScene();
-		
-		for (ObjectProperties obj : scene.getObjects()) 
-		{
-			//if (obj instanceof Circle) 
-			//{
-				obj.update();
-			//}
-		}
-	}
+    private Scene scene;
+    
+    CollisionDetector collDetector;
+    
+    public PhysicsEngine2D()
+    {
+        this.collDetector = new CollisionDetector(scene);
+        //
+    }
+    
+    public void Rotation()
+    {
+        //
+    }
+    
+    public void Translation()
+    {
+        //
+    }
+    
+    public void setScene(Scene scene)
+    {
+        this.scene = scene;
+        this.collDetector = new CollisionDetector(scene);
+    }
+    
+    // here starts the entry point for all the physical calculation
+    public void calculateNextFrame(double deltaTime)
+    {
+        EnvProps.deltaTime(deltaTime);
+        
+        collDetector.checkScene();
+        
+        for (ObjectProperties obj : scene.getObjects())
+        {
+            obj.update();
+        }
+    }
 }
