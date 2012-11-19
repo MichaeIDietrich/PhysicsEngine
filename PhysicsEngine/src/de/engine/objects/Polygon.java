e7a0efab57f3e9d887101bdebe353f8f544df347
Micha <micha@Micha-Laptop.de>
2012-11-19 12:13:53 +0100

    added a textbox to some decor; fixed a bug related to the aabb of a polygon

--- PhysicsEngine/src/de/engine/objects/Polygon.java
+++ PhysicsEngine/src/de/engine/objects/Polygon.java
@@ -6,40 +6,46 @@ import de.engine.math.Transformation;
 import de.engine.math.Util;
 import de.engine.math.Vector;
 
-public class Polygon extends ObjectProperties implements Cloneable {
-
-	public Vector[] points;
-	
-	public Polygon(Vector position) {
-		this.world_position = new Transformation(position, new Rotation(0));
-		radius = 0;
-	}
-
-	public Polygon(Vector position, double rotation) {
-		this.world_position = new Transformation(position, new Rotation(rotation));
-		radius = 0;
-	}
-	
-	public Polygon(Vector position, Vector[] points) {
-		this(position);
-		this.points = points;
-		calcRadius();
+public class Polygon extends ObjectProperties implements Cloneable
+{
+    public Vector[] points;
+    
+    public Polygon(Vector position)
+    {
+        this.world_position = new Transformation(position, new Rotation(0));
+        radius = 0;
+    }
+    
+    public Polygon(Vector position, double rotation)
+    {
+        this.world_position = new Transformation(position, new Rotation(rotation));
+        radius = 0;
+    }
+    
+    public Polygon(Vector position, Vector[] points)
+    {
+        this(position);
+        this.points = points;
+        calcRadius();
         calcMomentOfInertia();
-	}
-	
-	protected void calcRadius() {
-		radius = 0;
-		for (Vector point : points) {
-			double corner_dist = point.getLength();
-			radius = (corner_dist > radius) ? corner_dist : radius;
-		}
-	}
-	
-	protected void calcMomentOfInertia() {
-	    double zaehler = 0;
-	    double nenner = 0;
-	    
-	    for (int j = points.length - 1, i = 0; i < points.length; j = i, i++)
+    }
+    
+    protected void calcRadius()
+    {
+        radius = 0;
+        for (Vector point : points)
+        {
+            double corner_dist = point.getLength();
+            radius = (corner_dist > radius) ? corner_dist : radius;
+        }
+    }
+    
+    protected void calcMomentOfInertia()
+    {
+        double zaehler = 0;
+        double nenner = 0;
+        
+        for (int j = points.length - 1, i = 0; i < points.length; j = i, i++)
         {
             Vector p0 = points[j];
             Vector p1 = points[i];
@@ -50,27 +56,29 @@ public class Polygon extends ObjectProperties implements Cloneable {
             zaehler += (a * b);
             nenner += a;
         }
-	    moment_of_inertia = (mass / 6.0) * (zaehler / nenner);
-	}
-	
-	public Vector getWorldPointPos(int i) {
-		return world_position.getPostion(points[i]);
-	}
-	
-	public Vector getWorldPointPos(int i, double time) {
-	    double localtime = getTime(time);
-	    Transformation tr = new Transformation(Util.add(world_position.translation, Util.scale(velocity, localtime)), new Rotation(world_position.rotation.getAngle() + angular_velocity * localtime));
+        moment_of_inertia = (mass / 6.0) * (zaehler / nenner);
+    }
+    
+    public Vector getWorldPointPos(int i) {
+        return world_position.getPostion(points[i]);
+    }
+    
+    public Vector getWorldPointPos(int i, double time)
+    {
+        double localtime = getTime(time);
+        Transformation tr = new Transformation(Util.add(world_position.translation, Util.scale(velocity, localtime)), new Rotation(world_position.rotation.getAngle() + angular_velocity * localtime));
         return tr.getPostion(points[i]);
     }
-
-	@Override
-	public double getRadius() {
-		return radius;
-	}
-	
-	@Override
-	public void setRadius(double radius) {
-	    double scale = radius / getRadius();
+    
+    @Override
+    public double getRadius() {
+        return radius;
+    }
+    
+    @Override
+    public void setRadius(double radius)
+    {
+        double scale = radius / getRadius();
         for (Vector point : points) {
             point.scale(scale);
         }
@@ -112,12 +120,14 @@ public class Polygon extends ObjectProperties implements Cloneable {
     }
     
     @Override
-    public Vector[] getAABB() {
+    public Vector[] getAABB()
+    {
         return getAABB(0);
     }
     
     @Override
-    public Vector[] getAABB(double time) {
+    public Vector[] getAABB(double time)
+    {
         Vector aabb[] = new Vector[2];
         aabb[0] = new Vector(Double.MAX_VALUE, Double.MAX_VALUE);
         aabb[1] = new Vector(-1 * Double.MAX_VALUE, -1 * Double.MAX_VALUE);
@@ -128,30 +138,32 @@ public class Polygon extends ObjectProperties implements Cloneable {
                 v = getWorldPointPos(i);
             else
                 v = getWorldPointPos(i, time);
-            if(v.getX() > aabb[1].getX()) {
+            
+            if(v.getX() > aabb[1].getX())
                 aabb[1].setX(v.getX());
-            } else if(v.getX() < aabb[0].getX()) {
-                aabb[0].setX(v.getX());
-            }
-            if(v.getY() > aabb[1].getY()) {
+            
+            if(v.getY() > aabb[1].getY())
                 aabb[1].setY(v.getY());
-            } else if(v.getY() < aabb[0].getY()) {
+            
+            if(v.getX() < aabb[0].getX())
+                aabb[0].setX(v.getX());
+            
+            if(v.getY() < aabb[0].getY())
                 aabb[0].setY(v.getY());
-            }
         }
         return aabb;
     }
-
+    
     @Override
-    public Vector[] getNextAABB() {
+    public Vector[] getNextAABB()
+    {
         return getAABB(EnvProps.deltaTime());
     }
-
+    
     @Override
     public void setMass(double mass)
     {
         moment_of_inertia *= (mass / this.mass);
         this.mass = mass;
     }
-
 }
